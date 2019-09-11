da2378293ff02e4001a5bde2dbe771898bf5c492
Marciano Schildmeijer <marciano@kabisa.nl>
2019-09-11 21:35:13 +0200

    Setup fake db.

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,19 @@
-const Query = {};
+const Query = {
+  pokemon(parent, args, ctx, info) {
+    //   database calls
+    return [
+      {
+        name: "Charizard",
+        types: ["Fire/Flying"],
+        level: 36
+      },
+      {
+        name: "Blastoise",
+        types: ["Water"],
+        level: 36
+      }
+    ];
+  }
+};
 
 module.exports = Query;
